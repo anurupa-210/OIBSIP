0cddacb79ae569475c7dcb470b86cebeff052cb9
anurupa-210 <116992449+anurupa-210@users.noreply.github.com>
2023-07-15 18:19:15 +0530

    LEVEL 2 TASK 1

--- script.js
+++ script.js
@@ -1,47 +1,27 @@
-const converter = document.querySelector('#converter');
-const result = document.querySelector('.result');
-const convertButton = document.querySelector('.convertButton');
-const resetButton = document.querySelector('.resetButton');
-const changeButton = document.querySelector('.changeButton');
-const C = document.querySelector('.C')
-const F = document.querySelector('.F')
+let input = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
 
-const swap = () => {
-    if (C.innerHTML === '°C') {
-        C.innerHTML = '°F';
-        F.innerHTML = '°C';
-        converter.placeholder = "Temperature in °F";
-    }else {
-        F.innerHTML = '°F';
-        C.innerHTML = '°C';
-        converter.placeholder = "Temperature in °C";
-    };
-};
-
-const reset = () => {
-    converter.value = '';
-    result.innerHTML = '';
-}
-
-const convert = () => {
-    if (/^(-?)(\d+)(\.{1}\d+)?$/.test(converter.value)) {
-        result.style.color = "black"
-        if (C.innerHTML === '°C') {
-            let value = (converter.value * 1.8) + 32;
-            result.innerHTML = `${converter.value}°C is equal to ${value.toFixed(2)}°F`;
-        } else {
-            let value = (converter.value - 32) / 1.8;
-            result.innerHTML = `${converter.value}°F is equal to ${value.toFixed(2)}°C`;
+let string = "";
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click', (e) =>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            input.value = string;
         }
-    } else if (converter.value == '') {
-        result.style.color = "#993300"
-        result.innerHTML = 'Enter some number';
-    } else {
-        result.style.color = "#993300"
-        result.innerHTML = 'Enter only numbers!!!';
-    }
-}
 
-changeButton.addEventListener('click', swap);
-resetButton.addEventListener('click', reset);
-convertButton.addEventListener('click', convert);
+        else if(e.target.innerHTML == 'AC'){
+            string = "";
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'DEL'){
+            string = string.substring(0, string.length-1);
+            input.value = string;
+        }
+        else{
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+        
+    })
+})
